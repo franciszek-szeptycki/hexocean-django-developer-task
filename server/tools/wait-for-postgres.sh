9c7f452a9e4a34fd43d35ced52be79b9a956b655
franciszek szeptycki <franciszek.szeptycki@gmail.com>
2023-10-05 19:37:38 +0200

    containers 1.0

--- server/tools/wait-for-postgres.sh
+++ server/tools/wait-for-postgres.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db:5432" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
