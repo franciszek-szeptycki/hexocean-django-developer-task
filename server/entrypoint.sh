9f171ec76e52e5def400962da957fa83352a2532
franciszek szeptycki <franciszek.szeptycki@gmail.com>
2023-10-05 23:14:07 +0200

    example

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 python manage.py migrate
 python manage.py loaddata fixtures.json
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${ADMIN_USERNAME}', '', '${ADMIN_PASSWORD}')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${ADMIN_USERNAME}', '', '${ADMIN_PASSWORD}')" | python manage.py shell || true
 
 gunicorn hexocean.wsgi:application --bind 0.0.0.0:8000
