1594af302d6081a0e5631c37534054742587c55b
franciszek szeptycki <franciszek.szeptycki@gmail.com>
2023-10-05 22:46:25 +0200

    postgres + .env

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
-# set -e
-
-# until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db:3000" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-#   >&2 echo "Postgres is unavailable - sleeping"
-#   sleep 1
-# done
-
-# >&2 echo "Postgres is up - executing command"
+set -e
 
 python manage.py migrate
 python manage.py loaddata fixtures.json
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', '', 'admin')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${ADMIN_USERNAME}', '', '${ADMIN_PASSWORD}')" | python manage.py shell
 
 gunicorn hexocean.wsgi:application --bind 0.0.0.0:8000
